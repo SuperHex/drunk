d29024c2cd7250d1dd44c2987781edf7431eca2b
Soei Wong <wang_ruihan@hotmail.com>
2020-03-06 21:43:02 +0800

    export script

---
+++ src/nn/export.py
@@ -0,0 +1,14 @@
+import torch
+import torchvision
+import sys
+from models import MotionNet
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    output = sys.argv[2]
+    net = MotionNet(3)
+    net.load_state_dict(torch.load(name))
+    net.eval()
+    example = torch.rand(1, 6, 224, 224)
+    tracedModule = torch.jit.trace(net, example)
+    tracedModule.save(output)
