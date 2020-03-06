e0c1258bc80ebb086a57d56dea5966888e5bd644
Soei Wong <wang_ruihan@hotmail.com>
2020-03-07 00:26:07 +0800

    simple test program

--- src/frontend/src/fart.cpp
+++ src/frontend/src/fart.cpp
@@ -1,8 +1,23 @@
 #include "AudioFile.h"
-#include <torch/nn.h>
-#include <torch/torch.h>
+#include <torch/script.h>
 #include "opticalflow.hpp"
 
-int main() {
+#include <vector>
+#include <filesystem>
+
+torch::Tensor matToTensor(const cv::Mat& mat) {
+    auto tensor = torch::from_blob(mat.data, {1, mat.rows, mat.cols, 1}, at::kByte);
+    tensor = tensor.permute({{0, 3, 1, 2}});
+    return tensor;
+}
+
+int main(int argc, const char* argv[]) {
+    auto module = torch::jit::load(argv[1]);
+    std::cout << "Loaded model! " << std::endl;
+    std::vector<torch::jit::IValue> inputs(1, torch::ones({1, 6, 224, 224}));
+    auto out = module.forward(inputs).toTensor();
+    auto index = std::get<1>(torch::max(out, 0));
+    auto i = index.item<int>();
+    std::cout << "Inference: " << i << std::endl;
     return 0;
 }
