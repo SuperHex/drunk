f9168a42be5d3f1db175142980af2b455d9fe05b
Soei Wong <wang_ruihan@hotmail.com>
2020-03-07 15:49:21 +0800

    add optical flow

--- src/frontend/src/fart.cpp
+++ src/frontend/src/fart.cpp
@@ -5,18 +5,15 @@
 #include <vector>
 #include <filesystem>
 
-torch::Tensor matToTensor(const cv::Mat& mat) {
-    auto tensor = torch::from_blob(mat.data, {1, mat.rows, mat.cols, 1}, at::kByte);
-    tensor = tensor.permute({{0, 3, 1, 2}});
-    return tensor;
-}
-
 int main(int argc, const char* argv[]) {
     try {
+        std::string video(argv[2]);
+        opticalflow loader(video);
+        loader.preComputeAll();
         auto module = torch::jit::load(argv[1]);
         std::cout << "Loaded model! " << std::endl;
         std::vector<torch::jit::IValue> inputs(1, torch::ones({1, 6, 224, 224}));
-        auto out = module.forward(inputs).toTensor();
+        auto out = module.forward(inputs).toTensor().view(2).sub(0.5).div(0.5);
         auto index = std::get<1>(torch::max(out, 0));
         auto i = index.item<int>();
         std::cout << "Inference: " << i << std::endl;
--- src/frontend/src/opticalflow.hpp
+++ src/frontend/src/opticalflow.hpp
@@ -3,41 +3,96 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
-#include <queue>
-#include <vector>
-#include <memory>
+#include <torch/script.h>
 
-class lazyOpticalFlow {
-    lazyOpticalFlow(const std::string& file, size_t cacheSize) { loadVideo(file); }
+#include <chrono>
+#include <deque>
+#include <vector>
+
+#ifndef __clang__
+#include <execution>
+#endif
+
+torch::Tensor matToTensor(const cv::Mat& mat) {
+    auto tensor = torch::from_blob(mat.data, {1, mat.rows, mat.cols, 1}, at::kByte);
+    tensor = tensor.permute({{0, 3, 1, 2}});
+    return tensor;
+}
+
+class opticalflow {
+
+public:
+
+    opticalflow(const std::string& file) { loadVideo(file); }
 
     void loadVideo(const std::string& file) {
-        _capture = cv::VideoCapture{cv::samples::findFile(file)};
-        if (!_capture.isOpened()) {
+        capture = cv::VideoCapture{cv::samples::findFile(file)};
+        if (!capture.isOpened()) {
             std::cout << "Error open file: " << file << std::endl;
         }
     }
 
-    cv::Mat& operator()() {
-        if (!_flows.empty()) { _flows.pop(); }
-        size_t loop = _flows.empty() ? _cacheSize : 1;
+    size_t preComputeAll() {
+        std::vector<cv::Mat> buffer;
 
-        cv::Mat tmp, frame1, frame2;
-        _capture >> tmp;
-        cv::cvtColor(tmp, frame1, cv::COLOR_BGR2GRAY);
-        for (auto i = 0; i < loop; i++) {
-            _capture >> tmp;
-            cv::cvtColor(tmp, frame2, cv::COLOR_BGR2GRAY);
-            cv::Mat flow(frame1.size(), CV_32FC2);
-            cv::calcOpticalFlowFarneback(frame1, frame2, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
-            _flows.emplace(std::move(flow));
+        // assume at least one frame
+        auto startTime = std::chrono::steady_clock::now();
+        cv::Mat tmp, tmp2, frame;
+        while (true) {
+            capture >> tmp;
+
+            if (tmp.empty()) {
+                break;
+            }
+
+            cv::resize(tmp, tmp2, cv::Size(224, 224), 0, 0, cv::INTER_AREA);
+            cv::cvtColor(tmp2, frame, cv::COLOR_BGR2GRAY);
+            buffer.emplace_back(std::move(frame));
         }
+        auto endTime = std::chrono::steady_clock::now();
+        std::chrono::duration<double> diff = endTime - startTime;
+        std::cout << "Transform all images in " << diff.count() << " s" << std::endl;
 
-        cv::Mat& front = _flows.front();
-        return front;
+        std::vector<cv::Mat> flow_buffer;
+        flow_buffer.reserve(buffer.size() - 1);
+        startTime = std::chrono::steady_clock::now();
+        #if defined(__clang__)
+        std::transform(buffer.begin(), buffer.end() - 1, buffer.begin() + 1, std::back_inserter(flow_buffer),
+            [](const auto& frame1, const auto& frame2) {
+                cv::Mat flow(frame1.size(), CV_32FC2);
+                cv::calcOpticalFlowFarneback(frame1, frame2, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+                return flow;
+            } );
+        std::for_each(flow_buffer.begin(), flow_buffer.end(), [&](const auto& flow) {
+            cv::Mat parts[2];
+            cv::split(flow, parts);
+            flows_x.emplace_back(std::move(parts[0]));
+            flows_y.emplace_back(std::move(parts[1]));
+        });
+        #else
+        std::transform(std::execution::par, buffer.begin(), buffer.end() - 1, buffer.begin() + 1, std::back_inserter(flows),
+            [](const auto& frame1, const auto& frame2) {
+                cv::Mat flow(frame1.size(), CV_32FC2);
+                cv::calcOpticalFlowFarneback(frame1, frame2, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+                return flow;
+            } );
+        std::for_each(std::execution::par_seq, flow_buffer.begin(), flow_buffer.end(), [&](const auto& flow) {
+            cv::Mat parts[2];
+            cv::split(flow, parts);
+            flows_x.emplace_back(std::move(parts[0]));
+            flows_y.emplace_back(std::move(parts[1]));
+        });
+        #endif
+        endTime = std::chrono::steady_clock::now();
+        diff = endTime - startTime;
+        std::cout << "Precompute all optical flows in " << diff.count() << " s" << std::endl;
+        return flows_x.size();
     }
 
+
+
 private:
-    size_t _cacheSize;
-    cv::VideoCapture _capture;
-    std::queue<cv::Mat> _flows;
+    cv::VideoCapture capture;
+    std::deque<cv::Mat> flows_x;
+    std::deque<cv::Mat> flows_y;
 };
